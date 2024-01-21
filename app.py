61feb8c004a35fa37c0a46c3729a6c748cadc744
lukashevda <lukashevda@inbox.ru>
2024-01-22 00:25:47 +0300

    modified:   app.py 	modified:   db/__pycache__/db.cpython-312.pyc

--- app.py
+++ app.py
@@ -5,37 +5,42 @@ from db.db import User
 window = tk.Tk()
 window.geometry("960x640")
 
-frm_centre = tk.Frame(
+frm_login = tk.Frame(
     master=window,
-    borderwidth=1,
-    relief="solid",
+    borderwidth   =1,
+    relief        ="solid",
     highlightcolor="red",
-    border=5,
-    )
-
-frm_login = tk.Frame(master=frm_centre,
-                     width = 900,
-                     height = 900)
-
-lbl_login    = tk.Label(
-    master = frm_login,
-    text   = "Логин:",
-    width  = "20",
-    height = "5"
+    border        =1,
+    width         =300,
+    height        =300
 )
-ent_login    = tk.Entry(
-    master = frm_login,)
-lbl_password = tk.Label(
-    master = frm_login,text="Пароль:")
-ent_password = tk.Entry(
-    master = frm_login,)
-btn_login    = tk.Button(
-    master = frm_login,text = "Войти" )
-btn_exit     = tk.Button(
-    master = frm_login,text = "Выйти")
 
-frm_centre.pack(expand=1)
-frm_login.pack()
+lbl_login = tk.Label(
+    master =frm_login,
+    text   ="Логин:",
+    width  =20,
+    height =5
+)
+ent_login = tk.Entry(
+    master =frm_login,)
+lbl_password = tk.Label(
+    master =frm_login,
+    text   ="Пароль:"
+)
+ent_password = tk.Entry(
+    master = frm_login
+)
+btn_login = tk.Button(
+    master =frm_login,
+    text   ="Войти"
+)
+btn_exit = tk.Button(
+    master =frm_login,
+    text   ="Выйти"
+)
+
+frm_login.pack(anchor="nw")
+frm_login.pack_propagate(False)
 lbl_login.pack()
 ent_login.pack()
 lbl_password.pack()
@@ -44,21 +49,23 @@ btn_login.pack()
 btn_exit.pack()
 
 
+
 def login(event):
     lgn = ent_login.get()
     pwd = ent_password.get()
     user = User.authenticate(lgn, pwd)
     if user != None:
         if user.role == "Директор":
-            print("Директор")
+            frm_login.pack_forget()
         elif user.role == "Администратор":
-            print("Администратор")
+            frm_login.pack_forget()
         else:
             print(user.role)
     else:
         ent_password.delete(0,"end")
         ent_password.config(bg="#EE0000")
         window.focus()
+    
 
 
 btn_login.bind("<Button-1>",   login)
